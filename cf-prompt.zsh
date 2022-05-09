d59decfe3aa927d18ebb2de331316a07fe6d85c8
Mark Boyd <mark.boyd@gsa.gov>
2022-05-09 16:27:22 -0400

    update conditional handling

--- cf-prompt.zsh
+++ cf-prompt.zsh
@@ -1,17 +1,20 @@
+#!/bin/zsh
+
 function get_cloudfoundry_target_info() {
     CF_CONFIG=$(less ~/.cf/config.json)
     # CF_API_ENDPOINT=$(echo $CF_CONFIG | jq -r '(.Target | gsub("https://api.run.|.io";""))')
     CF_ORG=$(echo $CF_CONFIG | jq -r '.OrganizationFields.Name')
-    PROMPT_OUTPUT=$CF_ORG
+    PROMPT_OUTPUT="$CF_ORG"
     CF_SPACE=$(echo $CF_CONFIG | jq -r '.SpaceFields.Name')
-    if [ -z "$CF_SPACE" ]; then
+    if [[ -n "$CF_SPACE" ]]; then
         PROMPT_OUTPUT="${PROMPT_OUTPUT}:${CF_SPACE}"
     fi
+    echo "$PROMPT_OUTPUT"
 }
 
 function prompt_cloudfoundry_target() {
     PROMPT_OUTPUT=$(get_cloudfoundry_target_info)
-    if [[ -f "manifest.yml" ]] && [[ ! -z "$PROMPT_OUTPUT" ]]; then
+    if [[ -f "manifest.yml" ]] && [[ -n "$PROMPT_OUTPUT" ]]; then
         p10k segment -b black -f white -t "$PROMPT_OUTPUT" -i $'\Uf65e' 
     fi
 }
